0bb125581332f6ff434e886dd9d89f5fb8f07878
mathferret1013 <eregina92@gmail.com>
2016-03-10 18:34:26 -0500

    More debugging to do

--- Workbench/Program.cs
+++ Workbench/Program.cs
@@ -48,25 +48,6 @@
                     Console.ReadLine();
                 }
             }
-
-
-
-
-            /*
-            var points = new double[][]
-                             {
-                                 new double[] { 2, 3 },
-                                 new double[] { 5, 4 },
-                                 new double[] { 9, 6 },
-                                 new double[] { 4, 7 },
-                                 new double[] { 8, 1 },
-                                 new double[] { 7, 2 },
-                                 new double[] { 2, 2 }, 
-                             };
-
-            var tree = new KDTree(2, points);
-            var best = tree.FindNearestNeighbor(new double[] { 7, 10 });
-            */
         }
 
         static T[] GetNearest<T>(T[][] points, T[] sample, Func<T[], T[], double> metric)
