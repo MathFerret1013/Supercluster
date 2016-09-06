728d231c1cbfd62908d70d8d500a9c32a506f5e6
mathferret1013 <eregina92@gmail.com>
2016-09-05 21:11:48 -0400

    Added some documentation to MTree

--- Supercluster/Structures/MTree/MTree.cs
+++ Supercluster/Structures/MTree/MTree.cs
@@ -10,9 +10,27 @@
     using Supercluster.Structures.Interfaces;
 
     /// <summary>
-    /// 
+    ///
     /// </summary>
     /// <remarks>
+    /// 
+    /// MTrees have a number of properties that make them very attractive for use with large high dimensional data sets.
+    /// 
+    /// <list>
+    /// They are self balancing.
+    /// They do not require the items they index have a coordinate representation such as the <see cref="KDTree"/>.
+    /// They handle high-dimensional data quite well as they index new items using the relative distance to other items already in the tree.
+    /// This does not mean that MTrees are immune to the curse of dimensionality but they can efficiently handled data with 1000's of dimensions.
+    /// 
+    /// Draw Backs:
+    /// The MTree makes explicit use of properties of Metric Spaces. If your data set cannot be appropriately embedded into a metric space you cannot use an MTree.
+    /// For low dimension data sets with a coordinate representation, an MTree will be much slower than a KDTree (but still faster than a linear search).
+    /// 
+    /// MTrees are not easy to implement and their heavy reliance on Metric Space properties makes them quite difficult (if even possible) to modify for a non-metric space application.
+    /// 
+    /// 
+    /// </list>
+    /// 
     /// 
     /// References:
     /// [1] P. Ciaccia, M. Patella, and P. Zezula. M-tree: an efficient access method for similarity search in metric spaces. 
@@ -20,6 +38,7 @@
     /// 
     /// [2] Samet, H., Foundations of Multidimensional and Metric Data Structures 1st Ed., Elsevier/Morgan Kaufmann, 2006
     /// </remarks>
+    /// <seealso cref="Supercluster.KDTree"/>
     /// <typeparam name="T"></typeparam>
     public class MTree<T> : ISpatialQueryable<T>
     {
